1f7863b2441c35f1b3a8d744ce510c50b40e0411
sohahib <syaoda@yahoo.fr>
2023-07-06 21:29:17 -0400

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -1,11 +1,11 @@
 const React = require('react')
 const Default = require('./layouts/defaults')
-const Bread = require('../models/breads.js')
+//const Bread = require('../models/breads.js')
 
 
-function Index ({breads})  {
+function Index ({breads, title})  {
     return (
-      <Default>
+      <Default title={title}>
         {/* <h2>Index Page</h2>
         <p>I have {breads[1].name} bread!</p> */}
         {breads.map((bread, index)=>{
